34a832d75c913daaac26702cd2db97587a6a3fd6
Rexeth <106716657+Rexeth@users.noreply.github.com>
2022-09-16 10:44:01 +0800

    Add files via upload

--- Variable_Scope/App.java
+++ Variable_Scope/App.java
@@ -1,1 +1,15 @@
+public class App {
+
+    public static void main(String[] args) throws Exception {
+
+        // Local Variable
+        String myName = "Rexeth";
+
+        // "myName" can be printed inside this method
+        System.out.println(myName);
+    }
+
+    
+}
+
 
